07e11240849f49d070f0402853cd802394f1fb3e
Laisha <laishac00@gmail.com>
2023-12-18 17:44:37 -0600

    Clase el_juego_de_la_vida

---
+++ el_juego_de_la_.vida.rb
@@ -0,0 +1,17 @@
+class  Celula
+  def initialize(state, fila, columna)
+    @state = state
+    @fila = 4
+    @columna = 8
+
+
+  end
+  def contadorVivas
+
+  end
+end
+
+class Tablero
+  def initialize()
+
+  end
